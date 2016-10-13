eba41c1c0c96c209379944d41d53b3b022a58fb9
Nick Pisciotta <npisciotta@gmail.com>
2016-10-12 19:15:55 -0600

    Add payment api endpoint to route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   resources :loans, defaults: {format: :json}
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :loan, only: [:show] do
+        resources :payments, only: [:create] do
+        end
+      end
+    end
+  end
+
 end
