b76d88a82b5ed699631ec4d9ece73ae1982c9ecb
Nick Pisciotta <npisciotta@gmail.com>
2016-10-12 20:03:35 -0600

    Post requests can be made to create new payments

--- app/controllers/api_base_controller.rb
+++ app/controllers/api_base_controller.rb
@@ -1,10 +1,7 @@
 class ApiBaseController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def create
-    loan = Loan.find(params[:loan_id])
-    payment = loan.payments.new(payment_params)
-  end
+
 
 
 end
