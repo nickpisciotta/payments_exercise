cae97ffd02ccba9f3c5f38e67dd42f1c714e1b63
Nick Pisciotta <npisciotta@gmail.com>
2016-10-13 11:39:40 -0600

    Create show and index actions for payments Add payment date callback and request tests

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,10 @@ class Payment < ActiveRecord::Base
   validates_numericality_of :amount, :greater_than => 0
   validates :amount, presence: true
 
+  before_save :payment_date
+
+  def payment_date
+    write_attribute :date, Time.now.strftime("%d %B %Y")
+  end
+
 end
